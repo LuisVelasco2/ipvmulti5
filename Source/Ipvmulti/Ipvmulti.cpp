966dad01a1e8efe0ca681a60c00911048dcd0af7
LuisVelasco2 <133169809+LuisVelasco2@users.noreply.github.com>
2025-07-25 02:29:06 -0600

    no

--- Source/Ipvmulti/Ipvmulti.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Ipvmulti.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ipvmulti, "Ipvmulti" );
- 
